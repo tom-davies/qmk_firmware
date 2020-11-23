abfef715b48d0b902c9b993ac9c6ba81c7c51e8b
Tom Davies <git@070194.xyz>
2020-11-23 11:43:18 +0000

    Added 👀 and set default colours on boot

--- keyboards/massdrop/alt/keymaps/tom-davies/keymap.c
+++ keyboards/massdrop/alt/keymaps/tom-davies/keymap.c
@@ -8,6 +8,9 @@ enum alt_keycodes {
     DBG_KBD,               //DEBUG Toggle Keyboard Prints
     DBG_MOU,               //DEBUG Toggle Mouse Prints
     MD_BOOT,               //Restart into bootloader after hold timeout
+
+    WIDETXT,               // w i d e t e x t   f o r   a   w i d e   b o y
+    TAUNTXT,               // FoR ThE UlTiMaTe sHiTpOsTiNg eXpErIeNcE
 };
 
 #define TG_NKRO MAGIC_TOGGLE_NKRO //Toggle 6KRO / NKRO mode
@@ -15,8 +18,32 @@ enum alt_keycodes {
 
 #define COLEMAK 0
 #define QWERTY  1
-#define NUMPAD  2
-#define MODS    3
+#define MODS    2
+#define MEMES   3
+#define NUMPAD  4
+
+#undef UNICODE_KEY_WINC             // Key must be undefined before it can be redefined
+/*#define UNICODE_KEY_WINC KC_RCTL*/
+
+enum unicode_names {
+    ROFL,
+    SNEK,
+    THUM,
+    EYES,
+    OKAY,
+    BBBB,
+    SHRG
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+    [ROFL] = 0x1F602,   // 😂
+    [SNEK] = 0x1F40D,   // 🐍
+    [THUM] = 0x1F44D,   // 👍
+    [EYES] = 0x1F440,   // 👀
+    [OKAY] = 0x1F44C,   // 👌
+    [BBBB] = 0x1F171,   // 🅱
+    [SHRG] = 0x1F937    // 🤷
+};
 
 keymap_config_t keymap_config;
 
@@ -26,15 +53,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_NUHS, KC_HOME, \
         KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,          KC_ENT,  KC_PGUP, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RCTL, MO(3),   KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RCTL, MO(2),   KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [QWERTY] = LAYOUT(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_NUHS, KC_HOME, \
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RCTL, MO(3),   KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(2),   KC_LEFT, KC_DOWN, KC_RGHT
     ),
+    [MODS] = LAYOUT(
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE, \
+        _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, KC_PSCR, KC_SLCK, KC_PAUS, KC_NUBS, KC_END,  \
+        _______, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, KC_NLCK,          _______, KC_VOLU, \
+        _______, RGB_TOG, _______, _______, _______, MD_BOOT, TG_NKRO, DBG_TOG, _______, _______, _______, _______,          _______, KC_VOLD, \
+        DF(0),   DF(1),   _______,                            _______,                            _______, _______, MO(3),   _______, _______
+    ),
+    [MEMES] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, X(ROFL), _______, _______, _______, _______, _______, _______, _______, \
+        TAUNTXT, _______, _______, X(SNEK), X(THUM), _______, X(SHRG), _______, X(EYES), _______, X(OKAY), _______,          _______, _______, \
+        _______, _______, _______, _______, _______, X(BBBB), _______, _______, _______, _______, _______, _______,          _______, _______, \
+        _______, _______, _______,                            WIDETXT,                            _______, _______, _______, _______, _______
+    ),
+    /*
     [NUMPAD] = LAYOUT(
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_KP_7, KC_KP_8, KC_KP_9, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, \
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_KP_4, KC_KP_5, KC_KP_6, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, \
@@ -42,14 +84,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_KP_0, KC_PDOT, KC_PENT, XXXXXXX, _______,          _______, _______, \
         _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______
     ),
-    [MODS] = LAYOUT(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE, \
-        _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, KC_PSCR, KC_SLCK, KC_PAUS, KC_NUBS, KC_END,  \
-        _______, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, KC_NLCK,          _______, KC_VOLU, \
-        _______, RGB_TOG, _______, _______, _______, MD_BOOT, TG_NKRO, DBG_TOG, _______, _______, _______, _______,          KC_PGUP, KC_VOLD, \
-        DF(0),   DF(1),   _______,                            _______,                            TG(2),   _______, KC_HOME, KC_PGDN, KC_END
-    ),
-    /*
     [X] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
@@ -62,6 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
+    set_unicode_input_mode(UC_WINC);
 };
 
 // Runs constantly in the background, in a loop.
@@ -75,7 +110,55 @@ void matrix_scan_user(void) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     static uint32_t key_timer;
 
+    static struct {
+        bool on;
+        bool first;
+    } w_i_d_e_t_e_x_t = {false, false};
+
+    if (w_i_d_e_t_e_x_t.on) {
+        if (record->event.pressed) {
+            switch (keycode) {
+                case KC_A...KC_0:
+                case KC_SPC:
+                    if (w_i_d_e_t_e_x_t.first) {
+                        w_i_d_e_t_e_x_t.first = false;
+                    } else {
+                        send_char(' ');
+                    }
+                    break;
+                case KC_ENT:
+                    w_i_d_e_t_e_x_t.first = true;
+                    break;
+                case KC_BSPC:
+                    send_char('\b'); // backspace
+                    break;
+            }
+        }
+    }
+
+    static bool tAuNtTeXt = false;
+
+    if (tAuNtTeXt) {
+        if (record->event.pressed) {
+            if (keycode != KC_SPC)
+                tap_code(KC_CAPS);
+        }
+    }
+
     switch (keycode) {
+        /* z e s t y   m e m e s */
+        case WIDETXT:
+            if (record->event.pressed) {
+                w_i_d_e_t_e_x_t.on = !w_i_d_e_t_e_x_t.on;
+                w_i_d_e_t_e_x_t.first = true;
+            }
+            return false;
+        case TAUNTXT:
+            if (record->event.pressed) {
+                tAuNtTeXt = !tAuNtTeXt;
+            }
+            return false;
+
         case U_T_AUTO:
             if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
                 TOGGLE_FLAG_AND_PRINT(usb_extra_manual, "USB extra port manual mode");
